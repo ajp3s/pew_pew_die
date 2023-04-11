c9c611245649d43d7bbe2db6be56f7b1c9af6ce9
Antonio Peres <121125138+ajp3s@users.noreply.github.com>
2023-04-11 22:50:56 +0200

    light edit to improve readability

--- main.py
+++ main.py
@@ -4,32 +4,26 @@ import pygame
 pygame.font.init()
 pygame.mixer.init()
 
-# Loading sounds and adjusting volume.
+
 left_laser_sound = pygame.mixer.Sound(os.path.join("Assets", "laser_gun_sound_left.wav"))
 right_laser_sound = pygame.mixer.Sound(os.path.join("Assets", "laser_gun_sound_right.mp3"))
 right_laser_sound.set_volume(0.1)
 background_music = pygame.mixer.Sound(os.path.join("Assets", "general_battle.mp3"))
 background_music.set_volume(0.3)
 
-# Setting the game name.
+
 pygame.display.set_caption("Pew,pew,die!")
 
-# Storing ships measurements in a variable to use them later.
 ships_width, ships_height = 110, 110
 
-# And window measurements in constants.
 WIDTH, HEIGHT = 1600, 920
 
-# Creating border in the middle of the screen.
 BORDER = pygame.Rect(WIDTH / 2 - 5, 0, 10, HEIGHT)
 
-# Defining window size.
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Loading and resizing the background image to fit the window.
 BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join("Assets", "Outer-Space.jpg")), (WIDTH, HEIGHT))
 
-# Creating user events for when the ships take a hit.
 left_hit = pygame.USEREVENT + 1
 right_hit = pygame.USEREVENT + 2
 
@@ -37,10 +31,8 @@ FPS = 60
 projectile_speed = 12
 ships_speed = 8
 
-# Playing some music in the background,so we don't feel stupid while playing a 2D game.
 background_music.play(-1)
 
-# Loading, resizing and rotating the ships images.
 LEFT_SPACESHIP_IMAGE = pygame.image.load(
     os.path.join("Assets", "white_spaceship.png"))
 
@@ -53,7 +45,6 @@ RIGHT_SPACESHIP_IMAGE = pygame.image.load(
 RIGHT_SPACESHIP = pygame.transform.rotate(
     pygame.transform.scale(RIGHT_SPACESHIP_IMAGE, (ships_width, ships_height)), 90)
 
-# Choosing font.
 FONT = pygame.font.SysFont("Fixedsys Regular", 80)
 
 
@@ -100,21 +91,17 @@ def shooting_func(left_projectiles, right_projectiles, left, right):
 
 
 def draw_win(left, right, left_projectiles, right_projectiles, left_armor, right_armor):
-    # Drawing the background image and the border.
     WINDOW.blit(BACKGROUND, (0, 0))
     pygame.draw.rect(WINDOW, (0, 0, 0), BORDER)
 
-    # Drawing the armor points labels.
     left_armor_text = FONT.render("Bright Side: " + str(left_armor), True, (255, 255, 255))
     right_armor_text = FONT.render("Dark Side: " + str(right_armor), True, (255, 255, 255))
     WINDOW.blit(right_armor_text, (WIDTH - right_armor_text.get_width() - 20, 20))
     WINDOW.blit(left_armor_text, (20, 20))
 
-    # Drawing the ships.
     WINDOW.blit(LEFT_SPACESHIP, (left.x, left.y))
     WINDOW.blit(RIGHT_SPACESHIP, (right.x, right.y))
 
-    # Drawing projectiles.
     for projectile in left_projectiles:
         pygame.draw.rect(WINDOW, (110, 13, 129), projectile)
 
@@ -123,7 +110,6 @@ def draw_win(left, right, left_projectiles, right_projectiles, left_armor, right
     pygame.display.update()
 
 
-# Drawing the winner's phrase in the selected color.
 def endgame_draw(string, string1, string2, color):
     winner_text = FONT.render(string, True, color)
     winner_text1 = FONT.render(string1, True, color)
@@ -136,37 +122,29 @@ def endgame_draw(string, string1, string2, color):
 
 
 def main():
-    # Defining starting positions and size of the ships.
     left = pygame.Rect(150, 400, ships_width, ships_height)
     right = pygame.Rect(1350, 400, ships_width, ships_height)
 
-    # Gun clips.
     right_projectiles = []
     left_projectiles = []
 
-    # Starting armor points.
     left_armor = 10
     right_armor = 10
 
-    # Creating Clock object to limit the max FPS
     clock = pygame.time.Clock()
     executing = True
     while executing:
-        # Limiting the max FPS
         clock.tick(FPS)
         for event in pygame.event.get():
-            
-            # Closing the game if someone clicks on the "x" button.
+
             if event.type == pygame.QUIT:
                 executing = False
                 pygame.quit()
 
             if event.type == pygame.KEYDOWN:
-                # Defining starting point and size of the projectiles.
                 left_projectile = pygame.Rect(left.x + left.width - 10, left.y + ships_height // 2, 20, 5)
                 right_projectile = pygame.Rect(right.x, right.y + ships_height // 2, 20, 5)
 
-                # Defining shooting controls,limiting the maximum projectiles count and adding a sound to it.
                 if event.key == pygame.K_LCTRL and len(left_projectiles) < 3:
                     left_projectiles.append(left_projectile)
                     left_laser_sound.play()
@@ -185,7 +163,6 @@ def main():
         endgame_string1 = ""
         endgame_string2 = ""
 
-        # Finding out who won and picking their endgame phrase.
         if left_armor <= 0:
             endgame_string = "Dark side clouds everything..."
             endgame_string1 = "impossible to see the future is..."
@@ -199,11 +176,9 @@ def main():
             color = (240, 190, 99)
 
         if endgame_string != "":
-            # Calling the endgame_draw function to draw the chosen phrase on the screen.
             endgame_draw(endgame_string, endgame_string1, endgame_string2, color)
             break
-            
-        # Calling the other functions in our main function.
+
         shooting_func(left_projectiles, right_projectiles, left, right)
         keys_pushed = pygame.key.get_pressed()
         left_ship_controls(keys_pushed, left)
@@ -213,6 +188,5 @@ def main():
     main()
 
 
-# So the game can be executed from this file only.
 if __name__ == "__main__":
     main()
